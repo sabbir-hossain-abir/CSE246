ff489936cd776135a070e7a7ffa83e4a1b9d8949
Sabbir <2020-2-60-185@std.ewubd.edu>
2022-09-01 08:18:30 +0600

    added printing weight in 0/1 knapsack

--- 0_1_knapsack.c
+++ 0_1_knapsack.c
@@ -25,14 +25,29 @@ int knapSack(int W, int wt[], int val[], int n)
 		}
 	}
 
-	return V[n][W];
+	int res = V[n][W];   
+    printf("maximum value that can be obtained: %d\n", res);
+
+ 	w = W;
+
+ 	printf("Included Weights: ");
+    	for (i = n; i > 0 && res > 0; i--) {
+         
+        	if (res == V[i - 1][w])
+            	continue;       
+        	else {
+            	printf("%d ", wt[i - 1]);
+            	res = res - val[i - 1];
+            	w = w - wt[i - 1];
+        	}
+    	}
 }
 
 int main()
 {
-	int val[] = { 3, 4, 5, 6 };
-	int wt[] = { 2, 3, 4, 5 };
-	int W = 4;
+	int val[] = { 40, 100, 50, 60 };
+	int wt[] = { 20, 10, 40, 30 };
+	int W = 60;
 	int n = sizeof(val) / sizeof(val[0]);
 	printf("%d", knapSack(W, wt, val, n));
 	return 0;
